84f50a21c3d441490c6fddc5512606649682aefe
AyushVarshneylovesCoding <av5277670@gmail.comm>
2023-06-07 13:37:59 +0530

    basic-app-food-delivery

---
+++ backend/app.js
@@ -0,0 +1,74 @@
+const express =require("express");
+const app =express();
+const cors= require("cors");
+const dotenv =require("dotenv");
+const { application } = require("express");
+dotenv.config();
+app.use(cors());
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.post("/getReviews",(req,res)=>{
+console.log("getALlReviews",req.body.id)    
+let id=req.body.id
+const db = dbservice.getDbServiceInstance();
+
+const res1=db.getAllReviews(id)
+res1.then(data=>res.send(data))
+})
+
+app.post("/postReview",(req,res)=>{
+    console.log("getALlReviews",req.body.id,req,body.review)    
+    let id=req.body.id
+    let reviewText=req.body.review
+    const db = dbservice.getDbServiceInstance();
+    
+    const res1=db.getAllReviews(id)
+    var reviewsNew
+    res1.then(data=>reviewsNew=data+","+reviewText)
+    console.log(reviewsNew)
+
+    const res2=db.postReview(id,reviewsNew)
+    const res3=db.getAllReviews(id)
+    res3.then(data=>res.send(data))
+    })
+// create 
+app.post("/insert",(req,res)=>{
+    let id=req.body.id
+    let hotelName=req.body.name
+    let hotelRating =req.body.rating
+    let hotelImage=req.body.Image
+    console.log("insertRequestHandler")
+    const db = dbservice.getDbServiceInstance();
+
+    const res1=db.insertHotel(id,hotelName,hotelRating,hotelImage)
+    res1.then(data=>console.log(data))
+    console.log("inserted")
+})
+
+
+
+
+// read
+app.get("/getAll",(req,res)=>{
+    console.log("heregetAllRequest")
+    const db = dbservice.getDbServiceInstance();
+    const listOfRes=db.getAllData();
+   
+  listOfRes.then(data=>res.send(data))
+ 
+})
+
+
+
+//delete
+
+
+
+
+var mysql = require('mysql');
+const dbservice=require("./dbservice")
+
+
+app.listen(process.env.PORT,()=>{console.log("server is Running")})
---
+++ backend/dbservice.js
@@ -0,0 +1,114 @@
+const mysql=require("mysql")
+const dotenv=require("dotenv")
+const { response } = require("express")
+const connect = mysql.createConnection({
+    host:process.env.HOST_NAME,
+    user:process.env.USER_NAME,
+    port:process.env.PORT_NUMBER,
+    password:process.env.PASSWORD,
+    database:process.env.DATABASE
+})
+connect.connect((err)=>{
+    if(err){
+        console.log(err.message)
+    }
+    else{
+        console.log("db Connected")
+    }
+})
+let instance =null
+
+
+
+class DbService {
+    static getDbServiceInstance(){
+        return(instance?instance:new DbService())
+    }
+    async getAllData(){
+        try{
+                var response
+                response= await new Promise((resolve,reject)=>{
+                const query = "SELECT * from testtable"
+                connect.query(query,(err,result)=>{
+                    if(err){
+                        reject(new Error (err.message))
+                    }
+                    else{
+                        resolve(result)
+                    }
+                })
+            })
+
+        return(response)
+        }catch(error){
+            console.log(error)
+        }
+     
+    }
+
+    async insertHotel(a,b,c,d){
+        try{
+            const response1= await new Promise((resolve,reject)=>{
+                const query = `INSERT INTO testtable (id,name,rating,imageURL) VALUES (${a}, ${b},${c},${d} )  `
+                connect.query(query,(err,result)=>{
+                    if(err){
+                        reject(new Error (err.message))
+                    }
+                    else{
+                        resolve(result)
+                    }
+                })
+            })
+            return(response1)
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
+  async getAllReviews(a){
+    try{
+        console.log("hereAtgetALlREviews")
+        const response1= await new Promise((resolve,reject)=>{
+            const query = `SELECT * FROM reviews WHERE id=${a}`
+            connect.query(query,(err,result)=>{
+                if(err){
+                    reject(new Error (err.message))
+                }
+                else{
+                    resolve(result)
+                }
+            })
+        })
+    console.log(response1)
+        return(response1)
+    }
+    catch(error){
+        console.log(error)
+    }
+  }  
+
+
+  async postAReview(a,b){
+    try{
+        console.log("hereAtgetALlREviews")
+        const response1= await new Promise((resolve,reject)=>{
+            const query = ``
+            connect.query(query,(err,result)=>{
+                if(err){
+                    reject(new Error (err.message))
+                }
+                else{
+                    resolve(result)
+                }
+            })
+        })
+    console.log(response1)
+        return(response1)
+    }
+    catch(error){
+        console.log(error)
+    }
+  }  
+}
+module.exports=DbService
